4a155154843aa2f1faefea0a13d3d9d471c5a328
Christopher Beards <beardscm@gmail.com>
2018-08-09 14:05:39 -0400

    Added relationships, connected to mlab; removed user/password

--- server/app.js
+++ server/app.js
@@ -1,7 +1,38 @@
-const express = require('express');
-const app = express();
+//========================================
+//               IMPORTS
+//========================================
 
+const express = require('express');
+// * Used so express can understand GraphQL
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+// * Connect to DB
+const mongoose = require('mongoose');
+
+//========================================
+//              Server Setup
+//========================================
+const app = express();
 const PORT = 4000;
+
+// Connect to mlab database
+mongoose.connect(
+  'mongodb://<username>:<password>@ds215961.mlab.com:15961/gql-ninja',
+  { useNewUrlParser: true },
+);
+
+// Once the connection is open fire cb function
+mongoose.connection.once('open', () => {
+  console.log('Connected to database');
+});
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  }),
+);
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
